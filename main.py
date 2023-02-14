9d1c1c6d1c850e5a6af38ca7e941e2fe1a4e7bf4
Abhinav0909 <abhinavkarforma2@gmail.com>
2023-02-15 00:08:08 +0530

    ✨ Add import statements

---
+++ main.py
@@ -0,0 +1,17 @@
+!wget -q https://git.io/J0fjL -O IAM_Words.zip
+!unzip -qq IAM_Words.zip
+!
+!mkdir data
+!mkdir data/words
+!tar -xf IAM_Words/words.tgz -C data/words
+!mv IAM_Words/words.txt data
+from tensorflow.keras.layers.experimental.preprocessing import StringLookup
+from tensorflow import keras
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+import os
+
+np.random.seed(42)
+tf.random.set_seed(42)
