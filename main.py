8b6253038b426bf1fbe85aed3a768182147e2415
Aryaman Rana <aryamanrana34@gmail.com>
2023-02-15 00:35:42 +0530

    Building the data input pipeline

--- main.py
+++ main.py
@@ -14,3 +14,94 @@ import os
 
 np.random.seed(42)
 tf.random.set_seed(42)
+
+base_path = "data"
+words_list = []
+
+words = open(f"{base_path}/words.txt", "r").readlines()
+for line in words:
+    if line[0] == "#":
+        continue
+    if line.split(" ")[1] != "err":  # We don't need to deal with errored entries.
+        words_list.append(line)
+
+len(words_list)
+
+np.random.shuffle(words_list)
+
+split_idx = int(0.9 * len(words_list))
+train_samples = words_list[:split_idx]
+test_samples = words_list[split_idx:]
+
+val_split_idx = int(0.5 * len(test_samples))
+validation_samples = test_samples[:val_split_idx]
+test_samples = test_samples[val_split_idx:]
+
+assert len(words_list) == len(train_samples) + len(validation_samples) + len(
+    test_samples
+)
+
+print(f"Total training samples: {len(train_samples)}")
+print(f"Total validation samples: {len(validation_samples)}")
+print(f"Total test samples: {len(test_samples)}")
+
+base_image_path = os.path.join(base_path, "words")
+
+
+def get_image_paths_and_labels(samples):
+    paths = []
+    corrected_samples = []
+    for (i, file_line) in enumerate(samples):
+        line_split = file_line.strip()
+        line_split = line_split.split(" ")
+
+        # Each line split will have this format for the corresponding image:
+        # part1/part1-part2/part1-part2-part3.png
+        image_name = line_split[0]
+        partI = image_name.split("-")[0]
+        partII = image_name.split("-")[1]
+        img_path = os.path.join(
+            base_image_path, partI, partI + "-" + partII, image_name + ".png"
+        )
+        if os.path.getsize(img_path):
+            paths.append(img_path)
+            corrected_samples.append(file_line.split("\n")[0])
+
+    return paths, corrected_samples
+
+
+train_img_paths, train_labels = get_image_paths_and_labels(train_samples)
+validation_img_paths, validation_labels = get_image_paths_and_labels(validation_samples)
+test_img_paths, test_labels = get_image_paths_and_labels(test_samples)
+
+# Find maximum length and the size of the vocabulary in the training data.
+train_labels_cleaned = []
+characters = set()
+max_len = 0
+
+for label in train_labels:
+    label = label.split(" ")[-1].strip()
+    for char in label:
+        characters.add(char)
+
+    max_len = max(max_len, len(label))
+    train_labels_cleaned.append(label)
+
+characters = sorted(list(characters))
+
+print("Maximum length: ", max_len)
+print("Vocab size: ", len(characters))
+
+# Check some label samples.
+train_labels_cleaned[:10]
+
+def clean_labels(labels):
+    cleaned_labels = []
+    for label in labels:
+        label = label.split(" ")[-1].strip()
+        cleaned_labels.append(label)
+    return cleaned_labels
+
+
+validation_labels_cleaned = clean_labels(validation_labels)
+test_labels_cleaned = clean_labels(test_labels)
