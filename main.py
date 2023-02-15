bffd8e2cf6f898119d6de5d67cf1e56ab0a54aac
Abhinav0909 <abhinavkarforma2@gmail.com>
2023-02-16 02:18:25 +0530

    ✨ Add new features to give output

--- main.py
+++ main.py
@@ -105,3 +105,110 @@ def clean_labels(labels):
 
 validation_labels_cleaned = clean_labels(validation_labels)
 test_labels_cleaned = clean_labels(test_labels)
+
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+# Mapping characters to integers.
+char_to_num = StringLookup(vocabulary=list(characters), mask_token=None)
+
+# Mapping integers back to original characters.
+num_to_char = StringLookup(
+    vocabulary=char_to_num.get_vocabulary(), mask_token=None, invert=True
+)
+
+def distortion_free_resize(image, img_size):
+    w, h = img_size
+    image = tf.image.resize(image, size=(h, w), preserve_aspect_ratio=True)
+
+    # Check tha amount of padding needed to be done.
+    pad_height = h - tf.shape(image)[0]
+    pad_width = w - tf.shape(image)[1]
+
+    # Only necessary if you want to do same amount of padding on both sides.
+    if pad_height % 2 != 0:
+        height = pad_height // 2
+        pad_height_top = height + 1
+        pad_height_bottom = height
+    else:
+        pad_height_top = pad_height_bottom = pad_height // 2
+
+    if pad_width % 2 != 0:
+        width = pad_width // 2
+        pad_width_left = width + 1
+        pad_width_right = width
+    else:
+        pad_width_left = pad_width_right = pad_width // 2
+
+    image = tf.pad(
+        image,
+        paddings=[
+            [pad_height_top, pad_height_bottom],
+            [pad_width_left, pad_width_right],
+            [0, 0],
+        ],
+    )
+
+    image = tf.transpose(image, perm=[1, 0, 2])
+    image = tf.image.flip_left_right(image)
+    return image
+    batch_size = 64
+    padding_token = 99
+    image_width = 128
+    image_height = 32
+
+
+def preprocess_image(image_path, img_size=(image_width, image_height)):
+    image = tf.io.read_file(image_path)
+    image = tf.image.decode_png(image, 1)
+    image = distortion_free_resize(image, img_size)
+    image = tf.cast(image, tf.float32) / 255.0
+    return image
+
+
+def vectorize_label(label):
+    label = char_to_num(tf.strings.unicode_split(label, input_encoding="UTF-8"))
+    length = tf.shape(label)[0]
+    pad_amount = max_len - length
+    label = tf.pad(label, paddings=[[0, pad_amount]], constant_values=padding_token)
+    return label
+
+
+def process_images_labels(image_path, label):
+    image = preprocess_image(image_path)
+    label = vectorize_label(label)
+    return {"image": image, "label": label}
+
+
+def prepare_dataset(image_paths, labels):
+    dataset = tf.data.Dataset.from_tensor_slices((image_paths, labels)).map(
+        process_images_labels, num_parallel_calls=AUTOTUNE
+    )
+    return dataset.batch(batch_size).cache().prefetch(AUTOTUNE)
+    train_ds = prepare_dataset(train_img_paths, train_labels_cleaned)
+    validation_ds = prepare_dataset(validation_img_paths, validation_labels_cleaned)
+    test_ds = prepare_dataset(test_img_paths, test_labels_cleaned)
+
+    for data in train_ds.take(1):
+    images, labels = data["image"], data["label"]
+
+    _, ax = plt.subplots(4, 4, figsize=(15, 8))
+
+    for i in range(16):
+        img = images[i]
+        img = tf.image.flip_left_right(img)
+        img = tf.transpose(img, perm=[1, 0, 2])
+        img = (img * 255.0).numpy().clip(0, 255).astype(np.uint8)
+        img = img[:, :, 0]
+
+        # Gather indices where label!= padding_token.
+        label = labels[i]
+        indices = tf.gather(label, tf.where(tf.math.not_equal(label, padding_token)))
+        # Convert to string.
+        label = tf.strings.reduce_join(num_to_char(indices))
+        label = label.numpy().decode("utf-8")
+
+        ax[i // 4, i % 4].imshow(img, cmap="gray")
+        ax[i // 4, i % 4].set_title(label)
+        ax[i // 4, i % 4].axis("off")
+        plt.show()
