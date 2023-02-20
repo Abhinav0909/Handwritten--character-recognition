b61182b95c62f6f95f35034a252b1520f4f9da74
Abhinav0909 <abhinavkarforma2@gmail.com>
2023-02-20 20:37:09 +0530

    ✨ Add new features

--- main.py
+++ main.py
@@ -342,4 +342,57 @@ class EditDistanceCallback(keras.callbacks.Callback):
         print(
             f"Mean edit distance for epoch {epoch + 1}: {np.mean(edit_distances):.4f}"
         )
+
+        epochs = 10  # To get good results this should be at least 50.
+
+        model = build_model()
+        prediction_model = keras.models.Model(
+        model.get_layer(name="image").input, model.get_layer(name="dense2").output
+        )   
+        edit_distance_callback = EditDistanceCallback(prediction_model)
+
+        # Train the model.
+        history = model.fit(
+        train_ds,
+        validation_data=validation_ds,
+        epochs=epochs,
+        callbacks=[edit_distance_callback],
+        )
+        # A utility function to decode the output of the network.
+def decode_batch_predictions(pred):
+    input_len = np.ones(pred.shape[0]) * pred.shape[1]
+    # Use greedy search. For complex tasks, you can use beam search.
+    results = keras.backend.ctc_decode(pred, input_length=input_len, greedy=True)[0][0][
+        :, :max_len
+    ]
+    # Iterate over the results and get back the text.
+    output_text = []
+    for res in results:
+        res = tf.gather(res, tf.where(tf.math.not_equal(res, -1)))
+        res = tf.strings.reduce_join(num_to_char(res)).numpy().decode("utf-8")
+        output_text.append(res)
+    return output_text
+
+
+#  Let's check results on some test samples.
+    for batch in test_ds.take(1):
+    batch_images = batch["image"]
+    _, ax = plt.subplots(4, 4, figsize=(15, 8))
+
+    preds = prediction_model.predict(batch_images)
+    pred_texts = decode_batch_predictions(preds)
+
+    for i in range(16):
+        img = batch_images[i]
+        img = tf.image.flip_left_right(img)
+        img = tf.transpose(img, perm=[1, 0, 2])
+        img = (img * 255.0).numpy().clip(0, 255).astype(np.uint8)
+        img = img[:, :, 0]
+
+        title = f"Prediction: {pred_texts[i]}"
+        ax[i // 4, i % 4].imshow(img, cmap="gray")
+        ax[i // 4, i % 4].set_title(title)
+        ax[i // 4, i % 4].axis("off")
+
+        plt.show()
        
