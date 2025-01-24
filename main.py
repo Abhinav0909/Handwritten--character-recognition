84c13f31aa25c67ab519f810798f53993ccb950d
Abhinav0909 <abhinavkarforma2@gmail.com>
2025-01-25 00:19:47 +0530

    Uploaded the source point from kaggle to drive

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-!wget -q https://git.io/J0fjL -O IAM_Words.zip
-!unzip -qq IAM_Words.zip
-!
-!mkdir data
-!mkdir data/words
-!tar -xf IAM_Words/words.tgz -C data/words
-!mv IAM_Words/words.txt data
+#Modified from kaggle to google drive
+from google.colab import drive
+drive.mount('/content/drive')
+!cd /content/drive/MyDrive
+!unzip archive.zip
+!head -20 data/words.txt
 from tensorflow.keras.layers.experimental.preprocessing import StringLookup
 from tensorflow import keras
 import matplotlib.pyplot as plt
